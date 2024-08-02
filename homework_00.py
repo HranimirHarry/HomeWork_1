5c95654e6130643fd414d4176e71e981f0ccf950
HranimirHarry <pohari@gmail.com>
2024-08-02 17:56:32 +0200

    Homework-2
    H-2

---
+++ homework_00.py
@@ -0,0 +1,8 @@
+width = float(input("Please enter width :"))
+length = float(input("please enter length :"))
+
+area = width * length
+
+message = ("Area of the rectangle is " + str(area))
+
+print(message)
